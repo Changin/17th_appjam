0d98ab2c4088aab5b3c23b4607d20ed6042cf03e
ChangIn <qorckddls010@naver.com>
2018-12-22 20:55:18 +0900

    added login
    added login

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
+from django.http import *
 
 # Create your views here.
+@login_required
 def index(request):
-	return render(request, 'myapp/index.html', {})
+	username = request.user.username
+	return render(request, 'myapp/index.html', {'username' : username})
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render(request,'myapp/login.html')
